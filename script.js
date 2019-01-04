0c21b37d496cca8f7df11539578ad5398880d425
Dalton Saffe <daltonsaffe@gmail.com>
2019-01-04 13:22:02 -0500

    Add custom fonts

--- script.js
+++ script.js
@@ -5,4 +5,7 @@ const gradient = document.getElementById('gradient');
 arrow.addEventListener('click', function(e) {
   e.preventDefault();
   gradient.style.opacity = 0;
+  email.className = 'soon';
+  email.value = 'SOON THO';
+  email.style.opacity = 1;
 });
