835e17a6db19a872163693175ba72d52a9b643dd
Dalton Saffe <daltonsaffe@gmail.com>
2019-01-07 20:04:22 -0500

    timeOut events synced up

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const arrow = document.querySelector('#mc-embedded-subscribe');
 const email = document.querySelector('#mce-EMAIL');
 const gradient = document.querySelector('#gradient');
+const form = document.querySelector('#mc-embedded-subscribe-form');
 
-setTimeout(() => {
+window.setTimeout(() => {
   email.style.opacity = 1;
 }, 1500);
 
@@ -14,12 +15,21 @@ arrow.addEventListener('click', function(e) {
   e.preventDefault();
   arrow.style.opacity = 0;
   email.style.opacity = 0;
-  setTimeout(() => {
-    email.style.color = '#ffffff';
-    email.value = 'COMING SOON';
-    email.style.opacity = 1;
-  }, 2000);
-  setTimeout(() => {
+  const parent = document.querySelector('#mc_embed_signup');
+  const soon = document.createElement('h2');
+  window.setTimeout(() => {
     gradient.style.opacity = 0;
+  }, 2000);
+  window.setTimeout(() => {
+    parent.removeChild(form);
+    const textNode = document.createTextNode('COMING SOON');
+    soon.appendChild(textNode);
+    parent.appendChild(soon);
   }, 4000);
+  window.setTimeout(() => {
+    soon.style.opacity = 1;
+  }, 6000);
+  window.setTimeout(() => {
+    soon.style.opacity = 0;
+  }, 10000);
 });
