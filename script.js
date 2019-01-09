7b38cd2550db4b8aaf1ab38bcf2249c663be9cc1
Dalton Saffe <daltonsaffe@gmail.com>
2019-01-09 16:52:28 -0500

    Add style / timeOut mods

--- script.js
+++ script.js
@@ -6,12 +6,16 @@ const ggm = document.querySelector('#ggm-left');
 const ss19 = document.querySelector('#ss19-right');
 
 window.setTimeout(() => {
-  email.style.opacity = 1;
   ss19.style.opacity = 1;
   ggm.style.opacity = 1;
-}, 1500);
+}, 500);
+
+window.setTimeout(() => {
+  email.style.opacity = 1;
+}, 2000);
 
 const showArrow = () => {
+  email.placeholder = '';
   arrow.style.opacity = 1;
 };
 
@@ -20,22 +24,20 @@ arrow.addEventListener('click', function(e) {
   arrow.style.opacity = 0;
   email.style.opacity = 0;
   const parent = document.querySelector('#mc_embed_signup');
-  const soon = document.createElement('h2');
+  const soon = document.createElement('h3');
   window.setTimeout(() => {
     gradient.style.opacity = 0;
-  }, 1000);
+  }, 500);
   window.setTimeout(() => {
     parent.removeChild(form);
-    const textNode = document.createTextNode('COMING SOON');
+    const textNode = document.createTextNode('coming soon');
     soon.appendChild(textNode);
     parent.appendChild(soon);
   }, 3000);
   window.setTimeout(() => {
     soon.style.opacity = 1;
-  }, 3500);
+  }, 8000);
   window.setTimeout(() => {
     soon.style.opacity = 0;
-    ggm.style.opacity = 0;
-    ss19.style.opacity = 0;
-  }, 10000);
+  }, 12000);
 });
